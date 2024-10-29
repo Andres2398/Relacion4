b6ecdb9f169aba793e34418aa8a3afb5d66d71ab
Andres <andresbailo98n@gmail.com>
2024-10-29 13:07:15 +0100

    tablas

--- relacionejercicios4/src/tabla_n_numeros/Tabla2.java
+++ relacionejercicios4/src/tabla_n_numeros/Tabla2.java
@@ -1,10 +1,25 @@
 package tabla_n_numeros;
 
+import java.util.Scanner;
+
 public class Tabla2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Dame un numero para saber las tablas de multiplicar desde 1 hasta tu numero");
+		int numero = 0;
+		Scanner sc = new Scanner(System.in);
+		numero = sc.nextInt();
+		while (numero <= 1) {
+			System.out.println("Error, /n Dame un numero mayor a 1");
+			numero = sc.nextInt();
+		}
+			for (int i = 1; i <= numero; i++) {
+				System.out.println("Tabla de multiplicar de " + i);
+				for (int j = 1; j <= 10; j++) {
+				System.out.println(i + "*" + j + " = " + i * j);
+				}
+			}
+		}
 
 	}
 
-}
--- relacionejercicios4/src/tabla_un_numero/Tabla1.java
+++ relacionejercicios4/src/tabla_un_numero/Tabla1.java
@@ -1,10 +1,30 @@
 package tabla_un_numero;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Tabla1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Dame un numero para saber su tabla de mutiplicar");
+		int numero = 0;
+		Scanner sc = new Scanner(System.in);
+		numero = sc.nextInt();
+		while (numero<1){
+			System.out.println("Error \nDame un numero mayor a 0");
+			numero = sc.nextInt();
+			
+			
+		}
+		System.out.println("Tabla de multiplicar de " + numero);
+		for (int i = 1; i <= 10; i++) {
+			
+			System.out.println(numero + " * " + i +  " = " + numero * i );
+		}
+			
+		}
 
 	}
 
-}
+
