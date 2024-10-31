9b2f9bd9c5fbe2e8acf964d76555f81473c066aa
Andres <andresbailo98n@gmail.com>
2024-10-31 13:46:59 +0100

    Ambicioso corregido

--- relacionejercicios4/src/ambicioso/Ambicioso.java
+++ relacionejercicios4/src/ambicioso/Ambicioso.java
@@ -6,65 +6,65 @@ import java.util.Scanner;
 public class Ambicioso {
 
 	public static void main(String[] args) {
-		
+		boolean seguir=true;
 		int numero = 0;
-		boolean seguir = true;
 		boolean ambicioso = false;
 		int resultado = 0;
 		int perfecto = 0;
 		System.out.println("introduce un numero");
 		Scanner sc = new Scanner(System.in);
 		numero = sc.nextInt();
+		
+		for (int i = 1; i <= numero / 2 + 1; i++) {// comprobacion
+			if (numero % i == 0) { // perfecto
+				resultado += i;
+			}
+		}
 
-		while (seguir == true && ambicioso == false) {
-			for (int i = 2; i <= numero; i++) {
+		int comprobacion = 0;
+		if (numero == resultado) {
+			seguir = false;
 
-				if (numero % i == 0) {
-					resultado += i;
+		} else {
+
+			while (seguir == true && ambicioso == false) {
+				for (int i = 1; i <= resultado/2; i++) {	//sacar los divisores y sumarlos del resultado
+					if (resultado % i == 0) {
+						perfecto += i;
+					}
 				}
 
-			}
-			if (resultado == 0) {
-				seguir = false;
-			}
+				if (perfecto == resultado) {
+					ambicioso = true;
 
-			
+				} else {
+					for (int i = 2; i <= Math.sqrt(perfecto); i++) {// 
+						if (perfecto % i == 0) {					// comprobar si es primo 
+							comprobacion += i;						//
+						}
 
-			
+					}
+					if (comprobacion == 0) {
+						seguir = false;
 
-			for (int i = 2; i <= resultado; i++) {
-
-				if (resultado % i == 0) {
-					perfecto += i;
-
+					} else {
+						resultado = perfecto;
+						perfecto = 0;			//reset de valores
+						comprobacion = 0;
+					}
 				}
-
 			}
-			if (perfecto == 0) {
-				seguir = false;
-			}
-
-			if (perfecto == resultado) {
-				ambicioso = true;
-			} else {
-				resultado = perfecto;
-				perfecto = 0;
-			}
-
 		}
 
 		if (ambicioso == true) {
-			System.out.println(" es ambicioso ");
-
+			System.out.println("es ambicioso");
 		} else {
 			System.out.println("no es ambicioso");
 		}
-
-	}
-
-}
 		
-
+		
+		
+		
+	}
 	
-
-
+}
