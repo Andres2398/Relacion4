91bd6152622471fb7356e1c564ed1b7a1b2e4e77
1DAM-11 <1DAM-11@1ASIR-11.mshome.net>
2024-11-06 21:02:30 +0100

    algunos mas de todos

--- relacionejercicios4/src/todos/Todos.java
+++ relacionejercicios4/src/todos/Todos.java
@@ -3,44 +3,61 @@ package todos;
 import java.util.Scanner;
 
 public class Todos {
-	
+
 	static boolean primo(int numero) {
-		
+
 		boolean primo = true;
 		int i = 2;
-		while (i <= Math.sqrt(numero) && primo == true ) {
+		while (i <= Math.sqrt(numero) && primo == true) {
 			if (numero % i == 0) {
 				primo = false;
-
-			
-				
+			}
+			i++;
+		}
+		return primo;
+	}
+	
+	static boolean compuesto(int numero) {	
+		return !primo(numero);
+	}
+	static boolean perfecto(int numero) {
+		int perfecto = 0;
+		for (int i = 1; i <= numero/2+1; i++) {
+			if (numero % i == 0) {
+				perfecto = perfecto +  i;
 
 			}
-			i++;	
+			
+		}
+		boolean esperfecto=true;
+		if (perfecto == numero) {
+			esperfecto=true;
+		}else {
+			esperfecto=false;
 		}
 		
-		return primo;
+
+		return esperfecto;
 		
 	}
+
 	public static void main(String[] args) {
-		
-		int numero=0;
+
+		int numero = 0;
 		System.out.println("Introduce un numero");
 		Scanner sc = new Scanner(System.in);
 		numero = sc.nextInt();
-		
-		boolean primo = primo(numero); 	
-		
+
+		boolean primo = primo(numero);
+
 		System.out.println("el numero es primo ? " + primo);
 		
+		boolean compuesto=compuesto(numero);
+		System.out.println("el numero es compuesto? " + compuesto);
 		
-		
-		
-		
-		
+		boolean esperfecto= perfecto(numero);
+		System.out.println("el numero es perfecto? " + esperfecto);
+
 	}
 
-	
-	}
-
-
+}
